129caf406c35e8e5819e8150dec94e75e1e2c83a
Shuhaib T U <shuhaibtu79@gmail.com>
2023-10-26 13:24:12 +0530

    string problems completed

---
+++ Problems/String/string4.js
@@ -0,0 +1,13 @@
+/**
+ * string methods
+ */
+
+let str = "abcdas";
+
+// conscate method is used for concat to string.
+let str1 = str.concat("ba");
+// console.log(str1);
+
+// string replace method;
+let str2 = str.replace(str, "hello");
+ console.log(str2);
